ef205af3e2347c5836924397f049c968e345c584
Zach <zacharykrantz2@gmail.com>
2022-12-25 03:44:14 -0500

    Fix nav on mobile

--- src/NavBar.js
+++ src/NavBar.js
@@ -15,7 +15,7 @@ function NavBar( {title = "Z.A.M.", navElements} ) {
     useEffect(() => {
         const controlNavbar = () => {
             if(typeof window !== "undefined") {
-                if(window.scrollY > lastScrollY || window.scrollY === 0) {
+                if(window.scrollY > lastScrollY || Math.round(window.scrollY) === 0) {
                     setShow(false);
                 } else {
                     setShow(true);
